7162b2657437e5d8b24ec0c04a4bddd7df6c0ee7
deepaksalve <deepaksalve219@gmail.com>
2020-09-24 19:59:42 +0530

    Simple refactor

--- Backend/server.js
+++ Backend/server.js
@@ -1,174 +1,25 @@
-// Create express app
-var express = require("express")
-var app = express()
-var db = require("./database.js")
-var PORT = 8000
-var bodyParser = require("body-parser")
-const cors = require('cors')
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.json())
-// app.use(databaseMiddleware)
-app.use(cors())
+const routes = require("./routes");
+const PORT = 8000;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Root endpoint
+app.get("/", (req, res) => res.json({ message: "Ok" }));
+
+routes(app);
+
+// Default response for any other request
+app.use((req, res) => res.status(404));
 
 // Start server
 app.listen(PORT, () => {
-    console.log("Server running on port", PORT)
-});
-// Root endpoint
-app.get("/", (req, res, next) => {
-    return res.json({ "message": "Ok" })
-});
-
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Insert here other API endpoints
-app.get("/testsimulator", (req, res, next) => {
-    var sql = "select * from Vss2Capability"
-    var params = []
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-            return res.status(400).json({ "error": err.message });
-            return;
-        }
-        res.json({
-            "message": "success",
-            "data": rows
-        })
-    });
-});
-app.get("/value", (req, res, next) => {
-    var sql = "select * from Vss2Value where status = 'enabled'"
-    var params = []
-    db.all(sql, params, (err, rows) => {
-        if (err) {
-            return res.status(400).json({ "error": err.message });
-            return;
-        }
-        res.json({
-            "data": rows
-        })
-    });
-});
-
-app.get("/capability", (req, res, next) => {
-    var showSQL = "select Vss2Capability.signal,Description,Type,DataType,Unit,Min,Max,Enum,id,value,status from Vss2Capability  INNER JOIN Vss2Value  ON Vss2Capability.Signal = Vss2Value.Signal WHERE Vss2Value.status = 'enabled'";
-    var params = []
-    db.all(showSQL, params, (err, rows) => {
-        if (err) {
-            return res.status(400).json({ "error": err.message });
-            return;
-        }
-        res.json({
-            "data": rows
-        })
-    });
-});
-
-//post method
-app.post("/entry", (req, res) => {
-    var errors = []
-    var data = {
-        Signal: req.body.Signal,
-        Description: req.body.Description,
-        Type: req.body.Type,
-        DataType: req.body.DataType,
-        Unit: req.body.Unit,
-        Min: req.body.Min,
-        Max: req.body.Max,
-        Enum: req.body.Enum,
-        id: req.body.id,
-        value: req.body.value,
-        status: req.body.status
-    }
-
-    var entrySQL1 = 'INSERT INTO Vss2Capability (Signal, Description, Type, DataType, Unit, Min, Max, Enum) VALUES (?,?,?,?,?,?,?,?)'
-    var entryparams1 = [data.Signal, data.Description, data.Type, data.DataType, data.Unit, data.Min, data.Max, data.Enum]
-
-    db.run(entrySQL1, entryparams1, function (err, result) {
-        if (err) {
-            return res.status(400).json({ "error": err.message })
-        } else {
-            var entrySQL2 = 'INSERT INTO Vss2Value (Signal, id, value, status) VALUES (?,?,?,?)'
-            var entryparams2 = [data.Signal, data.id, data.value, data.status]
-
-            db.run(entrySQL2, entryparams2, function (err, result) {
-                if (err) {
-                    return res.status(400).json({ "error": err.message })
-                }
-                res.json({
-                    "data": data,
-                })
-            });
-        }
-    });
-    res.end('Cannot ' + req.method + ' ' + req.url);
-    console.log("data inserted")
-})
-
-//update the value
-app.put("/updatevalue/:Signal", (req, res) => {
-    var errors = [];
-    var data = {
-        Signal: req.body.Signal,
-        Description: req.body.Description,
-        Type: req.body.Type,
-        DataType: req.body.DataType,
-        Unit: req.body.Unit,
-        Min: req.body.Min,
-        Max: req.body.Max,
-        Enum: req.body.Enum,
-        id: req.body.id,
-        value: req.body.value,
-        status: req.body.status
-    }
-
-    var updateSQL1 = 'UPDATE Vss2Capability set  Signal = COALESCE(?,Signal), Description= COALESCE(?,Description), Type = COALESCE(?,Type), DataType = COALESCE(?,DataType), Unit = COALESCE(?,Unit),Min = COALESCE(?,Min),Enum = COALESCE(?,Enum), WHERE Signal = ?`,'
-    var updateparams1 = [data.Signal, data.Description, data.Type, data.DataType, data.Unit, data.Min, data.Max, data.Enum, req.params.Signal];
-    db.run(updateSQL1, updateparams1, function (err, result) {
-        if (err) {
-            res.status(400).json({ "error": res.message })
-            return;
-        } else {
-            var updateSQL2 = 'UPDATE Vss2Value set Signal = COALESCE(?,Signal), id = COALESCE(?,id), value = COALESCE(?,value) status = COALESCE(?,status) WHERE Signal = ?`'
-            var updateparams2 = [data.Signal, data.id, data.value, data.status]
-            db.run(updateSQL2, updateparams2, function (err, result) {
-                if (err) {
-                    res.status(400).json({ "error": res.message })
-                    return;
-                }
-                res.json({
-                    message: "success",
-                    data: data,
-                    changes: this.changes
-                })
-            });
-        }
-    });
-})
-
-//delete value
-app.delete("/deletedvalue/:signal", (req, res, next) => {
-    var deleteSQL1 = 'DELETE FROM Vss2Capability WHERE signal = ?'
-    db.run(deleteSQL1, req.params.signal, function (err, result) {
-        if (err) {
-            res.status(400).json({ "error": res.message })
-            return;
-        } else {
-            var deleteSQL2 = 'DELETE FROM Vss2Value WHERE signal = ?'
-            db.run(deleteSQL2, req.params.signal, function (err, result) {
-                if (err) {
-                    res.status(400).json({ "error": res.message })
-                    return;
-                }
-                res.json({ "message": "deleted", changes: this.changes })
-            });
-        }
-    });
-})
-
-
-// Default response for any other request
-app.use(function (req, res) {
-    res.status(404);
+  console.log("Server running on port", PORT);
 });
--- src/App.js
+++ src/App.js
@@ -1,166 +1,88 @@
-import React from 'react';
-import DataGrid, { Column, Editing } from 'devextreme-react/data-grid';
-import 'devextreme-react/text-area';
+import React from "react";
+import DataGrid, { Column, Editing } from "devextreme-react/data-grid";
+import "devextreme-react/text-area";
+import CustomStore from "devextreme/data/custom_store";
+import "whatwg-fetch";
 
-import CustomStore from 'devextreme/data/custom_store';
-// import { formatDate } from 'devextreme/localization';
-import 'whatwg-fetch';
-
-const URL = "http://127.0.0.1:8000";
+const API_ENDPOINT = "http://127.0.0.1:8000";
 
 class App extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { events: [] };
-  //   this.state = { vss2Value: [] };
-  // }
-
-  // componentDidMount() {
-  //   fetch(`http://127.0.0.1:8000/capability`)
-  //     .then(res => res.json())
-  //     .then((vss2Value) => {
-  //       this.setState({ vss2Value: vss2Value.data });
-  //     })
-  // }
-
   constructor(props) {
     super(props);
 
     this.state = {
       vss2Value: new CustomStore({
-        key: 'Signal',
-        load: () => this.sendRequest(`${URL}/capability`),
-        insert: (values) => this.sendRequest(`${URL}/entry`, 'POST', {
-          values: JSON.stringify(values)
-        }),
-        update: (key, values) => this.sendRequest(`${URL}/updatevalue`, 'PUT', {
-          key: key,
-          values: JSON.stringify(values)
-        }),
-        remove: (key) => this.sendRequest(`${URL}/deletedvalue`, 'DELETE', {
-          key: key
-        })
+        key: "Signal",
+        load: this.onLoad,
+        insert: this.onInsert,
+        update: this.onUpdate,
+        remove: this.onRemove,
       }),
       requests: [],
     };
   }
 
-  sendRequest(url, method, data) {
-    method = method || 'GET';
-    data = data || {};
-
-    // this.logRequest(method, url, data);
-
-    if (method === 'GET') {
-      return fetch(url, {
-        method: method,
-      }).then(result => result.json().then(json => {
-        if (result.ok) return json.data;
-        throw json.Message;
-      }));
-    }
-
-    const params = Object.keys(data).map((key) => {
-      return `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`;
-    }).join('&');
-
-    return fetch(url, {
-      method: method,
-      body: params,
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
-      },
-
-    }).then(result => {
-      if (result.ok) {
-        return result.text().then(text => text && JSON.parse(text));
-      } else {
-        return result.json().then(json => {
-          throw json.Message;
-        });
-      }
+  onLoad = () => {
+    return this.sendRequest("/capabilities");
+  };
+  onInsert = (values) => {
+    return this.sendRequest("/capabilities", "POST", values);
+  };
+  onUpdate = (key, values) => {
+    return this.sendRequest(`/capabilities/${key}`, "PUT", {
+      key,
+      values,
     });
-  }
+  };
+  onRemove = (key) => {
+    return this.sendRequest(`/capabilities/${key}`, "DELETE");
+  };
 
-  logRequest(method, url, data) {
-    const args = Object.keys(data || {}).map(function (key) {
-      return `${key}=${data[key]}`;
-    }).join(' ');
+  sendRequest(url, method, data) {
+    const config = {
+      method: method || "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
 
-    const request = [method, url.slice(URL.length), args].join(' ');
-  }
+    if (data) config.body = JSON.stringify(data);
 
-  handleRefreshModeChange(e) {
-    this.setState({ refreshMode: e.value });
+    return fetch(`${API_ENDPOINT}${url}`, config)
+      .then((resp) => resp.json())
+      .catch((err) => {
+        console.log("err =-----> ", err);
+      });
   }
 
   render() {
-    //const { vss2Value } = this.state;
-    const { refreshMode, vss2Value } = this.state
+    const { vss2Value } = this.state;
+
     return (
-      // <React.Fragment>
-      //   <form action="localhost:8000/entry" method="post">
-      //     <h2 style={{ color: "red" }}>{`Test Simulator App`}</h2>
-      //     <DataGrid
-      //       id="gridContainer"
-      //       dataSource={vss2Value}
-      //       remoteOperations={true}
-      //       // dataSource={dataSourceOptions}
-      //       
-      //       <Editing
-      //         mode="form"
-      //         allowUpdating={true}
-      //         allowDeleting={true}
-      //         allowAdding={true} />
-
-      //       <Column dataField="Signal" caption="Signal" />
-      //       <Column dataField="Description" />
-      //       <Column dataField="Type" />
-      //       <Column dataField="DataType" caption="Data Type" />
-      //       <Column dataField="Unit" />
-      //       <Column dataField="Min" caption="Minimum" />
-      //       <Column dataField="Max" caption="Maximum" />
-      //       <Column dataField="Enum" />
-      //       <Column dataField="id" caption="ID" />
-      //       <Column dataField="value" caption="Value" />
-      //       <Column dataField="status" caption="Status" />
-      //     </DataGrid>
-      //   </form>
-      // </React.Fragment>
-
       <React.Fragment>
-        <h2 style={{ color: "red" }}>{`Test Simulator App`}</h2>
-        <DataGrid
-          showBorders={true}
-          dataSource={vss2Value}
-          repaintChangesOnly={true}
-        >
+        <h2 style={{ color: "red" }}>Test Simulator App</h2>
+        <DataGrid showBorders dataSource={vss2Value} repaintChangesOnly>
           <Editing
-            refreshMode={refreshMode}
             mode="row"
-            allowAdding={true}
-            allowDeleting={true}
-            allowUpdating={true}
+            refreshMode
+            allowAdding
+            allowDeleting
+            allowUpdating
           />
 
-          <Column dataField="Signal" caption="Signal"></Column>
-          <Column dataField="Description"></Column>
-          <Column dataField="Type"></Column>
-          <Column dataField="DataType" caption="Data Type"></Column>
-          <Column dataField="Unit"></Column>
-          <Column dataField="Min" caption="Minimum"></Column>
-          <Column dataField="Max" caption="Maximum"></Column>
-          <Column dataField="Enum"></Column>
-          <Column dataField="id" caption="ID"></Column>
-          <Column dataField="value" caption="Value"></Column>
-          <Column dataField="status" caption="Status"></Column>
-
+          <Column dataField="Signal" caption="Signal" />
+          <Column dataField="Description" />
+          <Column dataField="Type" />
+          <Column dataField="DataType" caption="Data Type" />
+          <Column dataField="Unit" />
+          <Column dataField="Min" caption="Minimum" />
+          <Column dataField="Max" caption="Maximum" />
+          <Column dataField="Enum" />
+          <Column dataField="id" caption="ID" />
+          <Column dataField="value" caption="Value" />
+          <Column dataField="status" caption="Status" />
         </DataGrid>
-
-
       </React.Fragment>
-
-
     );
   }
 }
